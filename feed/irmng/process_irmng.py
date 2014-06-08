22263b880200f9f450c156f686156a43cfc61f88
Jonathan A Rees <jar398@mumble.net>
2014-06-07 20:08:54 -0400

    IRMNG improvements

--- feed/irmng/process_irmng.py
+++ feed/irmng/process_irmng.py
@@ -1,7 +1,15 @@
+# Command like arguments: something like
+#	   feed/irmng/in/IRMNG_DWC.csv
+#	   feed/irmng/in/IRMNG_DWC_SP_PROFILE.csv
+#	   tax/irmng/taxonomy.tsv
+#	   tax/irmng/synonyms.tsv
 
 import csv, string, sys
 
-# feed/irmng/in/IRMNG_DWC_20140113.csv
+irmng_file_name = sys.argv[1]
+profile_file_name = sys.argv[2]
+taxonomy_file_name = sys.argv[3]
+synonyms_file_name = sys.argv[4]
 
 taxa = {}
 synonyms = {}
@@ -12,11 +20,11 @@ synonyms = {}
 # 11 "NAMEPUBLISHEDIN","ACCEPTEDNAMEUSAGEID","PARENTNAMEUSAGE",
 # 14 "PARENTNAMEUSAGEID","TAXONREMARKS","MODIFIED","NOMENCLATURALCODE"
 
-with open(sys.argv[1], 'rb') as csvfile:
+with open(irmng_file_name, 'rb') as csvfile:
 	csvreader = csv.reader(csvfile)
 	header = csvreader.next()
 	if header[5] != 'FAMILY':
-		print '** Unexpected column name in header row', header[-3]
+		print >>sys.stderr, '** Unexpected column name in header row', header[-3]
 	for row in csvreader:
 		taxonid = row[0]
 		longname = row[1]
@@ -24,14 +32,15 @@ with open(sys.argv[1], 'rb') as csvfile:
 		genus = row[3]
 		family = row[5]
 		rank = row[6]
+		# TEMPORARY for compatibility with OTT 2.6
+		if rank == 'species': continue
 		status = row[7]
 		syn_target = row[12]
 		parent = row[-4]
 		if syn_target != '' and syn_target != taxonid:
 			synonyms[taxonid] = syn_target
-			continue
 #		if (status != '' and status != 'accepted' and status != 'valid' and 
-#		       status != 'available' and status != 'proParteSynonym'):
+#			   status != 'available' and status != 'proParteSynonym'):
 #			continue
 		if rank == 'genus':
 			name = genus
@@ -47,20 +56,20 @@ with open(sys.argv[1], 'rb') as csvfile:
 
 extinctp = {}
 
-not_extinct = ['1530',     # Actinopterygii
+not_extinct = ['1530',	   # Actinopterygii
 			   '1531',	   # Sarcopterygii
-			   '10565',    # Saurischia
+			   '10565',	   # Saurischia
 			   '11919',	   # Didelphimorphia
-			   #'1170022',  # Tipuloidea
-			   #'1340611',  # Retaria
-			   #'1124871',  # Labyrinthulomycetes [Labyrinthomorpha??]
-			   #'102024',   # Ophiurinidae - problem is Ophiurina
-			   #'1064058',  # Rhynchonelloidea genus/superfamily
+			   #'1170022',	# Tipuloidea
+			   #'1340611',	# Retaria
+			   #'1124871',	# Labyrinthulomycetes [Labyrinthomorpha??]
+			   #'102024',	# Ophiurinidae - problem is Ophiurina
+			   #'1064058',	# Rhynchonelloidea genus/superfamily
 			   '1021564',  # Cruciplacolithus
-			   #'1114655',  # Tetrasphaera - different from GBIF
+			   #'1114655',	# Tetrasphaera - different from GBIF
 			   ]
 
-with open(sys.argv[2], 'rb') as csvfile:
+with open(profile_file_name, 'rb') as csvfile:
 	csvreader = csv.reader(csvfile)
 	header = csvreader.next()
 	if header[1] != 'ISEXTINCT':
@@ -75,6 +84,7 @@ with open(sys.argv[2], 'rb') as csvfile:
 
 count = 0
 for taxonid in taxa:
+	if taxonid in synonyms: continue
 	(parent, name, rank) = taxa[taxonid]
 	if parent in synonyms:
 		parent = synonyms[parent]
@@ -85,21 +95,30 @@ for taxonid in taxa:
 				print >>sys.stderr, ("Non-extinct taxon with extinct parent: %s(%s) in %s(%s)"%
 									 (name, taxonid, taxa[parent][1], parent))
 			del extinctp[parent]
-			parent = taxa[parent][0]    # tuple (parent, name, rank)
+			parent = taxa[parent][0]	# tuple (parent, name, rank)
 			if parent in synonyms:
 				parent = synonyms[parent]
-print >>sys.stderr, "Non-extinct taxa with extinct parent:", count
+print >>sys.stderr, 'Non-extinct taxa with extinct parent:', count
 
-print '%s\t|\t%s\t|\t%s\t|\t%s\t|\t%s\t|\t'%('uid', 'parent_uid', 'name', 'rank', 'flags')
-print '%s\t|\t%s\t|\t%s\t|\t%s\t|\t%s\t|\t'%('0', '', 'life', 'no rank', '')
-for taxonid in taxa:
-	(parent, name, rank) = taxa[taxonid]
-	if parent in synonyms:
-		parent = synonyms[parent]
-	if parent == '':
-		parent = '0'
-	flags = ''
-	if taxonid in extinctp:
-		flags = 'extinct'
-	print '%s\t|\t%s\t|\t%s\t|\t%s\t|\t%s\t|\t'%(taxonid, parent, name, rank, flags)
+with open(taxonomy_file_name, 'w') as taxfile:
+	print 'Writing %s'%taxonomy_file_name
+	taxfile.write('%s\t|\t%s\t|\t%s\t|\t%s\t|\t%s\t|\t\n'%('uid', 'parent_uid', 'name', 'rank', 'flags'))
+	taxfile.write('%s\t|\t%s\t|\t%s\t|\t%s\t|\t%s\t|\t\n'%('0', '', 'life', 'no rank', ''))
+	for taxonid in taxa:
+		(parent, name, rank) = taxa[taxonid]
+		if parent in synonyms:
+			parent = synonyms[parent]
+		if parent == '':
+			parent = '0'
+		flags = ''
+		if taxonid in extinctp:
+			flags = 'extinct'
+		taxfile.write('%s\t|\t%s\t|\t%s\t|\t%s\t|\t%s\t|\t\n'%(taxonid, parent, name, rank, flags))
 
+with open(synonyms_file_name, 'w') as synfile:
+	print 'Writing %s'%synonyms_file_name
+	synfile.write('uid\t|\tname\t|\t\n')
+	for synid in synonyms:
+		(parent, name, rank) = taxa[synid]
+		targetid = synonyms[synid]
+		synfile.write('%s\t|\t%s\t|\t\n'%(targetid, name))

9588299ea673f6473c3033027fa152d55e6bf3c6
Jonathan A Rees <jar398@mumble.net>
2016-01-21 16:50:24 -0500

    server startup message

--- org/opentreeoflife/server/Services.java
+++ org/opentreeoflife/server/Services.java
@@ -62,6 +62,7 @@ public class Services {
     public void serve(String hostname, int port) throws IOException {
         final HttpServer server = HttpServer.create(new InetSocketAddress(hostname, port), BACKLOG);
         server.createContext("/conflict-status", conflictStatus);
+        System.out.format("Starting HTTP server\n");
         server.start();
     }
 
